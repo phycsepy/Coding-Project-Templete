1c384f84d55c1ff567bf2105cbdb5cec17d01cd8
phycsepy <sharma.saikundan@gmail.com>
2022-10-31 21:42:54 +0530

    fixed

--- trun.sh
+++ trun.sh
@@ -3,17 +3,27 @@
 #1  = project_name
 #2 = project_desc
 
-
 mkdir $1
 
 cp Templet/* $1
 
+
+mv $1 ../
+
+cd ..
+
+rm -rf Coding-Project-Templete
+
+
+
+
+
 cd $1
 echo " Project Name : $1" >> README
 
-echo " Date : `date +%d%b`" >> README
+echo " Date : `date +%b%d`" >> README
 
 if [[ $2 != "" ]]
 then
-echo "Description : $2"  >> README
+echo " Description : $2"  >> README
 fi
